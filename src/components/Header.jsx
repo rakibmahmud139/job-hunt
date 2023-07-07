25a7dec169706aa7448d963b0da49880a4805f46
Rakibmahmud <hasansaikat74@gmail.com>
2023-07-08 00:26:34 +0600

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,37 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
+            <div className='my-container flex items-center bg-gray-100'>
+                <div className='mt-12 px-24'>
+                    <h3 className='font-bold text-5xl text-gray-900'>One Step Closer To Your <span className='text-cyan-300 mt-3'>Dream Job</span></h3>
+                    <p className='text-gray-500 mt-5 mb-5'>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                    <button className='btn-outlined'>Get Started</button>
+                </div>
 
+                <img src="/public/fahim.jpg.png" alt="cover" />
+            </div>
+
+
+            {/* Job Category */}
+
+            <div className='my-container'>
+                <h3 className='font-bold text-3xl text-gray-900 mb-5'>Job Category List</h3>
+                <p className='text-gray-500'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+
+                <div>
+
+                </div>
+            </div>
+
+
+            {/* Jobs Features */}
+
+            <div className='my-container'>
+                <h3 className='font-bold text-3xl text-gray-900 mb-5'>Featured Jobs</h3>
+                <p className='text-gray-500'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
         </div>
+
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <nav className=' flex justify-between items-center p-2 rounded shadow-md bg-gray-100'>
-            <Link to='/'>
-                <h1 className='font-semibold text-3xl text-gray-900'>JobHunt</h1>
-            </Link>
+        <nav className=' flex justify-between items-center p-4 bg-gray-100'>
+            <NavLink
+                to='/'
+                className={({ isActive }) => (isActive ? 'active' : 'default')}>
+                <h1 className='font-bold text-3xl'>JobHunt</h1>
+            </NavLink>
 
-            <div className='flex gap-12 text-gray-500'>
-                <Link to='/statistics'>
+            <div className='flex gap-12'>
+                <NavLink
+                    to='/statistics'
+                    className={({ isActive }) => (isActive ? 'active' : 'default')}>
                     <p>Statistics</p>
-                </Link>
-                <Link to='applied-jobs'>
+                </NavLink>
+
+                <NavLink to='applied-jobs'
+                    className={({ isActive }) => (isActive ? 'active' : 'default')}>
                     <p>Applied Jobs</p>
-                </Link>
-                <Link to='blogs'>
+                </NavLink>
+
+                <NavLink to='blogs'
+                    className={({ isActive }) => (isActive ? 'active' : 'default')}>
                     <p>Blog</p>
-                </Link>
+                </NavLink>
             </div>
 
-            <button className='btn-primary'>Star Applying</button>
+            <button className='btn-primary'>Start Applying</button>
         </nav>
     );
 };
